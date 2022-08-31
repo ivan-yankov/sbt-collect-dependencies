377b6d4048a5ee611ac6a013f9f13027a35223cf
Ivan <ivan.iankov@gmail.com>
2022-08-31 16:49:03 +0300

    Initial commit

---
+++ src/main/scala/yankov/sbt/plugin/SbtCollectDependencies.scala
@@ -0,0 +1,45 @@
+package yankov.sbt.plugin
+
+import sbt.Keys.fullClasspath
+import sbt.{Def, File, taskKey, *}
+
+import java.nio.file.{Files, Path, Paths}
+import scala.collection.JavaConverters.*
+
+object SbtCollectDependencies extends AutoPlugin {
+  private def collectJars(files: Seq[Path], destDir: Path): Unit = {
+    files.filter(x => x.toString.endsWith(".jar")).foreach(
+      x => {
+        val src = x
+        val dest = Paths.get(destDir.toString, x.getFileName.toString)
+        Files.copy(src, dest)
+      }
+    )
+  }
+
+  object autoImport {
+    val collectDependencies = taskKey[Unit]("collect dependencies")
+    val jarDir = settingKey[String]("destination jar directory")
+  }
+
+  import autoImport.*
+
+  private val defaultSettings: Seq[Def.Setting[?]] = Seq(
+    jarDir := "jar",
+
+    collectDependencies := {
+      val jarPath = Paths.get(jarDir.value)
+      if (!Files.exists(jarPath)) Files.createDirectories(jarPath)
+
+      val cp: Seq[File] = (Runtime / fullClasspath).value.files
+      collectJars(cp.map(x => x.toPath), jarPath)
+      collectJars(Files.walk(Paths.get("target", "scala-2.12"), 1).iterator().asScala.toList, jarPath)
+    }
+  )
+
+  override def trigger = allRequirements
+
+  override lazy val projectSettings: Seq[Def.Setting[?]] = defaultSettings
+
+  override lazy val buildSettings: Seq[Def.Setting[?]] = defaultSettings
+}
